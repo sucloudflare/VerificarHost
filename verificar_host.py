c4514cf3fe87db05ae583f384e053574e902b17f
Edson Bruno <135662753+sucloudflare@users.noreply.github.com>
2023-12-10 09:14:58 -0300

    Update verificar_host.py

--- verificar_host.py
+++ verificar_host.py
@@ -1,7 +1,6 @@
 # Importa todas as funcionalidades da biblioteca Scapy
 from scapy.all import *
 
-
 verificarhost_logo = """\033[33m
    ▄█    █▄       ▄████████  ▄████████    ▄█   ▄█▄  ▄█  ███▄▄▄▄      ▄██████▄           
   ███    ███     ███    ███ ███    ███   ███ ▄███▀ ███  ███▀▀▀██▄   ███    ███      
@@ -22,26 +21,12 @@ print(verificarhost_logo)
 hosts = input("Digite os hosts a serem analisados (separados por vírgula): ").split(",")
 
 # Constrói um pacote IP com um segmento TCP para realizar a varredura de portas
-pacote = IP(dst=hosts)/TCP(dport=(
-pacote = IP(dst=hosts)/TCP(dpo
-
-pacote = IP(dst=host
-
-pacote = IP(d
-1,500), flags="S")
+pacote = IP(dst=hosts)/TCP(dport=(1, 500), flags="S")
 
 # Envia o pacote construído e aguarda por respostas, com um timeout de 1 segundo
-respondidos, nao_respondidos = sr(pacote, timeout=
-respondidos, nao_respondidos = sr(pacote, t
-
-respondidos, nao
-
-res
-1)
+respondidos, nao_respondidos = sr(pacote, timeout=1)
 
 # Itera sobre as respostas recebidas
 for n in range(len(respondidos)):
     # Imprime o endereço IP de destino e a porta de destino para cada resposta
-    
-    pr
-print("{} -> {}".format(respondidos[n][0][IP].dst, respondidos[n][0][TCP].dport))
+    print("{} -> {}".format(respondidos[n][0][IP].dst, respondidos[n][0][TCP].dport))
