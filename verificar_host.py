62bd9430ce242040bcd96e9987626a55e7ecc8be
Edson Bruno <135662753+sucloudflare@users.noreply.github.com>
2023-12-10 09:43:14 -0300

    Update verificar_host.py

--- verificar_host.py
+++ verificar_host.py
@@ -1,38 +1,46 @@
 # Importa todas as funcionalidades da biblioteca Scapy
 from scapy.all import *
 
-verificarhost_logo = """\033[93m
-   ▄█    █▄       ▄████████  ▄████████    ▄█   ▄█▄  ▄█  ███▄▄▄▄      ▄██████▄           
-  ███    ███     ███    ███ ███    ███   ███ ▄███▀ ███  ███▀▀▀██▄   ███    ███      
-  ███    ███     ███    ███ ███    █▀    ███▐██▀   ███▌ ███   ███   ███    █▀          
- ▄███▄▄▄▄███▄▄   ███    ███ ███         ▄█████▀    ███▌ ███   ███  ▄███                
-▀▀███▀▀▀▀███▀  ▀███████████ ███        ▀▀█████▄    ███▌ ███   ███ ▀▀███ ████▄           
-  ███    ███     ███    ███ ███    █▄    ███▐██▄   ███  ███   ███   ███    ███         
-  ███    ███     ███    ███ ███    ███   ███ ▀███▄ ███  ███   ███   ███    ███          
-  ███    █▀      ███    █▀  ████████▀    ███   ▀█▀ █▀    ▀█   █▀    ████████▀          
-                                         ▀                                                                            ▀                             
-                                    \033[94m[✔] https://github.com/sucloudflare/VerificarHost   [✔]
-                                    \033[94m[✔]            Version 1.1               [✔]
-                                    
-\033[97m"""
+def verificar_hosts():
+    verificarhost_logo = """\033[93m
+       ▄█    █▄       ▄████████  ▄████████    ▄█   ▄█▄  ▄█  ███▄▄▄▄      ▄██████▄           
+      ███    ███     ███    ███ ███    ███   ███ ▄███▀ ███  ███▀▀▀██▄   ███    ███      
+      ███    ███     ███    ███ ███    █▀    ███▐██▀   ███▌ ███   ███   ███    █▀          
+     ▄███▄▄▄▄███▄▄   ███    ███ ███         ▄█████▀    ███▌ ███   ███  ▄███                
+    ▀▀███▀▀▀▀███▀  ▀███████████ ███        ▀▀█████▄    ███▌ ███   ███ ▀▀███ ████▄           
+      ███    ███     ███    ███ ███    █▄    ███▐██▄   ███  ███   ███   ███    ███         
+      ███    ███     ███    ███ ███    ███   ███ ▀███▄ ███  ███   ███   ███    ███          
+      ███    █▀      ███    █▀  ████████▀    ███   ▀█▀ █▀    ▀█   █▀    ████████▀          
+                                             ▀                                                                            ▀                             
+                                        \033[94m[✔] https://github.com/sucloudflare/VerificarHost   [✔]
+                                        \033[94m[✔]            Version 1.1               [✔]
+                                        
+    \033[97m"""
 
-print(verificarhost_logo)
+    print(verificarhost_logo)
 
-# Lista de hosts que serão analisados
-hosts = input("Digite os hosts a serem analisados (separados por vírgula): ")
+    while True:
+        # Lista de hosts que serão analisados
+        hosts = input("Digite os hosts a serem analisados (separados por vírgula): ")
 
-# Verifica se o input está vazio
-if not hosts:
-    print("\033[91m[!] Hosts não fornecidos. Digite pelo menos um host.\033[97m")
-    exit()
+        # Verifica se o input está vazio
+        if not hosts:
+            print("\033[91m[!] Hosts não fornecidos. Digite pelo menos um host.\033[97m")
+            continue
 
-# Constrói um pacote IP com um segmento TCP para realizar a varredura de portas
-pacote = IP(dst=hosts)/TCP(dport=(1, 500), flags="S")
+        # Constrói um pacote IP com um segmento TCP para realizar a varredura de portas
+        pacote = IP(dst=hosts)/TCP(dport=(1, 500), flags="S")
 
-# Envia o pacote construído e aguarda por respostas, com um timeout de 1 segundo
-respondidos, nao_respondidos = sr(pacote, timeout=1)
+        # Envia o pacote construído e aguarda por respostas, com um timeout de 1 segundo
+        respondidos, nao_respondidos = sr(pacote, timeout=1)
 
-# Itera sobre as respostas recebidas
-for n in range(len(respondidos)):
-    # Imprime o endereço IP de destino e a porta de destino para cada resposta
-    print("{} -> {}".format(respondidos[n][0][IP].dst, respondidos[n][0][TCP].dport))
+        # Itera sobre as respostas recebidas
+        for n in range(len(respondidos)):
+            # Imprime o endereço IP de destino e a porta de destino para cada resposta
+            print("{} -> {}".format(respondidos[n][0][IP].dst, respondidos[n][0][TCP].dport))
+
+        novamente = input("Deseja verificar hosts novamente? (s/n): ").lower()
+        if novamente != 's':
+            break
+
+verificar_hosts()
